6960b5eeccaf9cbb9265415af304c844c44c468c
kjh <wogks8329@naver.com>
2023-11-16 14:33:12 +0900

    찾아오는 길 팝업 크기 설정 제한

--- iroom_mall/src/main/webapp/js/header/header.js
+++ iroom_mall/src/main/webapp/js/header/header.js
@@ -6,7 +6,7 @@ function fnShowLocationPopup(){
 	var popUrl = "/location/location.do";
 	var _left = Math.ceil((window.screen.width - 550)/2);
 	var _top = Math.ceil((window.screen.height - 450)/2);
-	var popOption = "width =550px, height=450px, left=" + _left + ", top=" + _top;
+	var popOption = "width =550px, height=450px, left=" + _left + ", top=" + _top + "resizable = no, location=no, menubar=no, toolbar=no";
 	
 	window.open(popUrl, "찾아오는 길", popOption);
 }
